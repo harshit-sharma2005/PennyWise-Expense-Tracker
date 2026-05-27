2cb6d025cbdf9049b864a0448dbf9ab8b5eb5a4e
harshitsharma2005 <work.harshitsharma8899@gmail.com>
2026-05-27 23:16:03 +0530

    defined routes

---
+++ backEnd/server.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express")
+const cors=require("cors")
+const path=require("path")
+const connectDB =require('./config/db')
+const authRoutes=require("./routes/authRoutes")
+const incomeRoutes=require("./routes/incomeRoutes")
+const expenseRoutes=require("./routes/expenseRoutes")
+const dashboardRoutes=require("./routes/dashboardRoutes")
+
+
+const app=express()
+
+//middlewares
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET" , "POST", "PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"],
+    })
+)
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth",authRoutes)     //path ek baar dekhna padega
+app.use("/api/v1/income",incomeRoutes)
+app.use("/api/v1/expense",expenseRoutes)
+app.use("/api/v1/dashboard",dashboardRoutes)
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>console.log(`Server is ruuning on ${PORT}`))
